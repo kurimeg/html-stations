0e11934633799553da8185e43170deb16a9f82d5
Megumi Kurihara <pomme.c5.musique@gmail.com>
2021-08-25 13:35:48 +0900

    modified station14

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -6,10 +6,15 @@ function getData() {
         { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
         { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
     ];
-    return test.map(buildFullName)
+    return buildFullName(test)
 }
 
 function buildFullName(data) {
+    data.map(item => {
+        item.full_name = item.family_name + " " + item.first_name
+        return item
+    })
+    return data
     // Station14の問題はこちらです。想定する答えをここを書いてください。
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
